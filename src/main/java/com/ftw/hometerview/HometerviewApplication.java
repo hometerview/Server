4e0e4567afd4b3542d54d5b5023355b3fda9fbd4
rachel5004 <j26597719@naver.com>
2022-07-09 23:06:35 +0900

    INIT: initial commit

---
+++ src/main/java/com/ftw/hometerview/HometerviewApplication.java
@@ -0,0 +1,13 @@
+package com.ftw.hometerview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HometerviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HometerviewApplication.class, args);
+	}
+
+}
