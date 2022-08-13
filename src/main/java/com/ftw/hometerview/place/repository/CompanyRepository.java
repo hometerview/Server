5c47f64e315d9ddc579abfa68c23c03952b0830a
rachel3486 <rachel3486@gmail.com>
2022-08-14 07:08:20 +0900

    feat: Add manual company registration API

---
+++ src/main/java/com/ftw/hometerview/place/repository/CompanyRepository.java
@@ -0,0 +1,10 @@
+package com.ftw.hometerview.place.repository;
+
+import com.ftw.hometerview.place.domain.Company;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CompanyRepository extends MongoRepository<Company, String> {
+
+}
