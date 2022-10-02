4beab8ac47a12a04294c260de3fa130ccbe40139
code5753 <code.57x53@gmail.com>
2022-10-02 20:14:37 +0900

    feat: Add auth dto files

---
+++ src/main/java/com/ftw/hometerview/auth/controller/dto/LoginResponse.java
@@ -0,0 +1,17 @@
+package com.ftw.hometerview.auth.controller.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class LoginResponse {
+
+    private String accessToken;
+    private String refreshToken;
+}
---
+++ src/main/java/com/ftw/hometerview/auth/controller/dto/ProviderType.java
@@ -0,0 +1,12 @@
+package com.ftw.hometerview.auth.controller.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum ProviderType {
+    KAKAO, APPLE;
+
+
+}
