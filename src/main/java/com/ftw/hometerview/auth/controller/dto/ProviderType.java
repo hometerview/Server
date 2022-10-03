d437f77742260a0069ad2ef6da0b383fbc638a28
code5753 <code.57x53@gmail.com>
2022-10-03 21:11:35 +0900

    refactor: Remove RequiredArgsConstructor annotation
    ref: #33

--- src/main/java/com/ftw/hometerview/auth/controller/dto/ProviderType.java
+++ src/main/java/com/ftw/hometerview/auth/controller/dto/ProviderType.java
@@ -1,12 +1,8 @@
 package com.ftw.hometerview.auth.controller.dto;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
 public enum ProviderType {
     KAKAO, APPLE;
-
-
 }
