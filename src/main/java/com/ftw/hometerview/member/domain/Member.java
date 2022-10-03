dba10fec739f3d9d55c5cee477212f91fc1e508d
code5753 <code.57x53@gmail.com>
2022-10-03 22:28:33 +0900

    refactor: Remove unused NoArgs, AllArgs annotations
    ref: #33

--- src/main/java/com/ftw/hometerview/member/domain/Member.java
+++ src/main/java/com/ftw/hometerview/member/domain/Member.java
@@ -2,21 +2,14 @@ package com.ftw.hometerview.member.domain;
 
 import com.ftw.hometerview.auth.controller.dto.ProviderType;
 import com.ftw.hometerview.core.domain.AbstractDocument;
-import java.time.LocalDateTime;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.bson.types.ObjectId;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.MongoId;
 
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
 @Getter
 @Document(collection = "member")
 @FieldDefaults(level = AccessLevel.PRIVATE)
