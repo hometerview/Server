8bad25928f3c38d91277d2295aa842032266586a
code5753 <code.57x53@gmail.com>
2022-10-03 21:19:01 +0900

    refactor: Modify mongo repository id
    ref: #33

--- src/main/java/com/ftw/hometerview/member/repository/MemberRepository.java
+++ src/main/java/com/ftw/hometerview/member/repository/MemberRepository.java
@@ -2,13 +2,11 @@ package com.ftw.hometerview.member.repository;
 
 import com.ftw.hometerview.member.domain.Member;
 import java.util.Optional;
-import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MemberRepository extends MongoRepository<Member, ObjectId> {
+public interface MemberRepository extends MongoRepository<Member, String> {
 
     Optional<Member> findByMemberId(String memberId);
 }
