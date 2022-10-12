ffb2d5eec0241dd0be036d44e3b2dba9bbc8c7c8
rachel3486 <rachel3486@gmail.com>
2022-10-12 23:34:55 +0900

    feat: Add station name to CompanyDto

--- src/main/java/com/ftw/hometerview/place/repository/station/StationRepository.java
+++ src/main/java/com/ftw/hometerview/place/repository/station/StationRepository.java
@@ -1,10 +1,13 @@
 package com.ftw.hometerview.place.repository.station;
 
 import com.ftw.hometerview.place.domain.Station;
+import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StationRepository extends MongoRepository<Station, String>,StationRepositoryCustom {
+
+    List<Station> findByIds(List<String> ids);
 
 }
