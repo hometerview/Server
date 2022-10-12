036d6f65ef54c16e01e89cfc390ccb2546f202fa
rachel3486 <rachel3486@gmail.com>
2022-10-13 00:27:23 +0900

    feat: Add String to SearchType converter

--- src/main/java/com/ftw/hometerview/core/config/WebConfig.java
+++ src/main/java/com/ftw/hometerview/core/config/WebConfig.java
@@ -1,7 +1,9 @@
 package com.ftw.hometerview.core.config;
 
 import com.ftw.hometerview.core.interceptor.AuthorizationInterceptor;
+import com.ftw.hometerview.core.util.SearchTypeConverter;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -14,6 +16,11 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new AuthorizationInterceptor())
             .addPathPatterns("/api/**");
+    }
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(new SearchTypeConverter());
     }
 
 }
